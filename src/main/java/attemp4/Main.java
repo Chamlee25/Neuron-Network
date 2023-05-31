02957c21042b8caa1c808eb0fb52169d3fd8b0fd
Chamlee25 <fandicek7@gmail.com>
2023-05-31 21:43:23 +0200

    Hidden layer learning added
    nefunguje, 1 pokus ano, ale 60000 pokusů způsobí že si síť řekne "input" = 9 nebo 10 a na nic jinýho nehraju

--- src/main/java/attemp4/Main.java
+++ src/main/java/attemp4/Main.java
@@ -4,22 +4,39 @@ import Mnist.Mnist;
 import Mnist.MnistLoader;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Network net = new Network();
         MnistLoader ml = new MnistLoader();
-        Mnist m = ml.getNextPictureData();
+        Mnist m;
+        /*
         double[] test = net.testOutput(m.data);
         for(int i =0; i<10; i++){
             System.out.println("Case "+ i + ": " + String.format("%.15f",test[i]));
         }
         System.out.println(m.actualvalue);
-        net.learn(m.data,m.actualValue,1);
+        net.learn(m.data,m.actualValue,0.01);
         double[] test2 = net.testOutput(m.data);
         for(int i =0; i<10; i++){
             System.out.println("Case "+ i + ": " + String.format("%.15f",test2[i]));
         }
+        */
+        for(int i =0; i<60000; i++){
+            m = ml.getNextPictureData();
+            net.learn(m.data,m.actualValue,0.001);
+            if(i%2000==0){
+                double[] x = net.testOutput(m.data);
+                for(int j =0; j<10; j++){
+                    System.out.println("Case "+ i + ": " + String.format("%.15f",x[j]));
+                }
+                System.out.println(m.actualvalue);
+            }
+        }
+
+
+
 
 
 
--- src/main/java/attemp4/Network.java
+++ src/main/java/attemp4/Network.java
@@ -28,15 +28,32 @@ public class Network {
         return last;
     }
 
+
+
     public void learn(double[] input, double[] actualValue, double learningRate){
         double[] first = layers[0].layerFeedForward(input,false);
         double[] hidden = layers[1].layerFeedForwardFromPrevious(first,true);
         double[] last = layers[2].layerFeedForwardFromPrevious(hidden,false);
         last = Neuron.softmax(last);
-        for(int i =0; i<10; i++){
-            this.layers[2].deltas[i] = actualValue[i]-last[i] * Neuron.softmaxDerivative(last)[i];
-            this.layers[2].neurons[i].updateLastNeuron(this.layers[2].deltas[i],learningRate);
+
+        //teaching last layer
+
+        for(int i =0; i<10; i++) {
+            this.layers[2].deltas[i] = actualValue[i] - last[i] * Neuron.softmaxDerivative(last)[i];
+            this.layers[2].neurons[i].updateLastNeuron(this.layers[2].deltas[i], learningRate);
         }
+
+
+        //teaching middle layer
+        for(int i =0; i<256; i++){
+            double deltaSum =0;
+            for(int j =0; j<10; j++){
+                deltaSum -= this.layers[2].deltas[j]*this.layers[2].neurons[j].weights[i];
+            }
+
+            this.layers[1].neurons[i].updateLastNeuron(deltaSum,learningRate);
+        }
+
 
     }
 
